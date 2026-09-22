37f14f94f261c5412c9f0c83b6091406327388cd
Cole <aderibigbeadedamolajohn@gmail.com>
2026-09-22 09:54:36 +0100

    refactor: simplify Analytics script syntax and enable noEmit in TypeScript configuration

--- docs/app/components/analytics.tsx
+++ docs/app/components/analytics.tsx
@@ -1,12 +1,9 @@
 export function Analytics() {
   return (
     <>
-      <script
-        dangerouslySetInnerHTML={{
-          __html:
-            "window.va = window.va || function () { (window.vaq = window.vaq || []).push(arguments); };",
-        }}
-      />
+      <script>
+        {"window.va = window.va || function () { (window.vaq = window.vaq || []).push(arguments); };"}
+      </script>
       <script defer src="/_vercel/insights/script.js" />
     </>
   );
